bbd7135759a6f4bbc0fb57f12a3306983ea711e6
Fede Folmer <fedefolmer00@gmail.com>
2023-03-24 13:41:49 -0300

    titulo

--- src/components/Inicio/Inicio.js
+++ src/components/Inicio/Inicio.js
@@ -12,7 +12,7 @@ const Inicio = () => {
     return (
         <div className='inicio-container'>
             <Portada subtitulo={"Decoración y reflexión..."} />
-            <h2>A tu casa no le puede faltar un Micromundo...
+            <h2 className="inicio-titulo">A tu casa no le puede faltar un Micromundo...
                 
             </h2>
             <Slider imgT={images} />
