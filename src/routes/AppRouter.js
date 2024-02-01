1d69b30503a78996fe151d5d5a09a6440bf9a58e
Fede Folmer <fedefolmer00@gmail.com>
2024-02-01 14:40:06 -0300

    rutes publics

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,24 +1,25 @@
 import { BrowserRouter } from "react-router-dom";
-import { useContext } from 'react';
-import { LoginContext } from '../Context/LoginContext';
-import PrivateRutes from './PrivateRutes';
+// import { useContext } from 'react';
+// import { LoginContext } from '../Context/LoginContext';
+// import PrivateRutes from './PrivateRutes';
 import PublicRutes from "./PublicRutes";
 
 
 
 const AppRouter = () => {
 
-    const {user}= useContext(LoginContext)
+    // const {user}= useContext(LoginContext)
 
     return (
 
         <BrowserRouter>
 
-            {
-                user.logged
-                    ? <PrivateRutes/>
-                    : <PublicRutes/>
-            }
+            // {
+            //     user.logged
+            //         ? <PrivateRutes/>
+                    // : 
+        <PublicRutes/>
+            // }
 
         </BrowserRouter>
     )
